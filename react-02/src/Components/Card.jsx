f3476ca989cb1e1d8b1d26ca15dce5cc4e490deb
kannan05-m <kannanmehra05@gmail.com>
2026-06-05 12:34:55 +0530

    components in react

---
+++ react-02/src/Components/Card.jsx
@@ -0,0 +1,12 @@
+
+
+function Card () {
+    return (
+        <div className= "card">
+        <h1>Kannan Mehra</h1>
+        <h2>Java Developer</h2>
+       </div>
+        
+    )
+}
+export default Card
